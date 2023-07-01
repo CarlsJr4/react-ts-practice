9989bb779bff2f984dc4043505515439a59ba6e8
Carl Dungca <carl.dungca@yahoo.com>
2023-06-30 18:47:27 -0700

    Mosh state exercise 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // Task: Click the button to update the name
+
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: 'John',
+    },
+  });
+
+  const handleClick = () => {
+    setGame({ ...game, player: { ...game.player, name: 'Carl' } });
+  };
+
   return (
     <div className="App">
-      <p>Test</p>
+      <p>{game.player.name}</p>
+      <button onClick={handleClick}>Change game</button>
     </div>
   );
 }
--- src/components/test.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import AppProps from '../types/test';
-
-export default function test({ message }: AppProps) {
-  return <div>{message}</div>;
-}
